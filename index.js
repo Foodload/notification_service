1e35af9fb7e92781f6348ab9472192bf53d72049
Antonio <antonio_97@live.se>
2020-12-21 15:04:44 +0100

    added move_item message type

--- index.js
+++ index.js
@@ -66,6 +66,8 @@ sub.on('message', function (_channel, data) {
     io.local.to(parsedData.familyId).emit('update_item', data);
   } else if (messageType == 'family_invite') {
     io.local.to(parsedData.userId).emit('family_invite', data);
+  } else if (messageType == 'move_item') {
+    io.local.to(parsedData.familyId).emit('update_item', data);
   } else if (messageType == 'change_family') {
     io.local
       .of('/')
